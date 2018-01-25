f24b280e4f3b92d001cce0835ad3d78d72e74bf4
wang-sai <532969033@qq.com>
2018-01-25 14:48:45 +0800

    add signin code

--- routes/signout.js
+++ routes/signout.js
@@ -5,7 +5,10 @@ const checkLogin = require('../middlewares/check').checkLogin
 
 // GET /signout 登出
 router.get('/', checkLogin, (req, res, next) => {
-  res.send('登出')
+  req.session.user = null
+  req.flash('success', '登出成功')
+  // 登出成功后跳转到主页
+  res.redirect('/posts')
 })
 
 module.exports = router
