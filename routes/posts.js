3eb1b5bc86595d769a89778e6a1810900d3909ed
wang-sai <532969033@qq.com>
2018-01-26 14:40:08 +0800

    add post create and content

--- routes/posts.js
+++ routes/posts.js
@@ -1,27 +1,83 @@
 const express = require('express')
 const router = express.Router()
+const PostModel = require('../models/posts')
 
 const checkLogin = require('../middlewares/check').checkLogin
 
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
 router.get('/', (req, res, next) => {
-  res.render('posts')
+  const author = req.query.author
+
+  PostModel.getPosts(author)
+    .then((posts) => {
+      res.render('posts', {
+        posts
+      })
+    })
+    .catch(next)
 })
 
 // POST /posts/create 发表一篇文章
 router.post('/create', checkLogin, (req, res, next) => {
-  res.send('发表文章')
+  const author = req.session.user._id
+  const title = req.fields.title
+  const content = req.fields.content
+
+  // 校验参数
+  try {
+    if (!title.length) {
+      throw new Error('请填写标题')
+    }
+    if (!content.length) {
+      throw new Error('请填写内容')
+    }
+  } catch (e) {
+    req.flash('error', e.message)
+    return res.redirect('back')
+  }
+
+  let post = {
+    author,
+    title,
+    content
+  }
+
+  PostModel.create(post)
+    .then((result) => {
+      // 此 post 是插入 mongodb 后的值，包含 _id
+      post = result.ops[0]
+      req.flash('success', '发表成功')
+      // 发表成功后跳转到该文章页
+      res.redirect(`/post/${post._id}`)
+    })
+    .catch(next)
 })
 
 // GET /posts/create 发表文章页
 router.get('/create', checkLogin, (req, res, next) => {
-  res.send('发表文章页')
+  res.render('create')
 })
 
 // GET /posts/:postId 单独一篇的文章页
 router.get('/:postId', (req, res, next) => {
-  res.send('文章详情页')
+  const postId = req.params.postId
+
+  Promise.all([
+    PostModel.getPostById(postId), // 获取文章信息
+    PostModel.incPv(postId) // pv 加 1
+  ])
+    .then((result) => {
+      const post = result[0]
+      if (!post) {
+        throw new Error('该文章不存在')
+      }
+
+      res.render('post', {
+        post
+      })
+    })
+    .catch(next)
 })
 
 // GET /posts/:postId/edit 更新文章页
